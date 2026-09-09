42b103aa1e54633e684e1e048cda7f371edd24b8
Toyobijutsu-Prt <hatanaka@toyobijutsu-prt.co.jp>
2026-09-09 17:07:33 +0900

    html追加

---
+++ index.php
@@ -0,0 +1,340 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
+<meta charset="utf-8"/>
+<meta name="viewport" content="width=device-width, user-scalable=yes"/>
+<title>千代田観光まちづくり実行委員会 千代田区の観光、商業振興、地域活性化に向けた活動をしています</title>
+<meta name="keywords" content="千代田観光まちづくり実行委員会,千代田区,観光,千代田のさくらまつり,無料シャトルバス,さくら祭り号"/>
+<meta name="description" content="千代田観光まちづくり実行委員会は、千代田区の商工業者と街の活性化に向けて取り組んでおります。"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+<meta name="twitter:card" content="summary_large_image" />
+<meta property="og:title" content="千代田観光まちづくり実行委員会"/>
+<meta property="og:type" content="website"/>
+<meta property="og:description" content="千代田観光まちづくり実行委員会は、千代田区の商工業者と街の活性化に向けて取り組んでおります。" />
+<meta property="og:url" content="https://www.kanmachi.com/"/>
+<meta property="og:image" content="https://www.kanmachi.com/img/common/logo.png"/>
+<link rel="icon" href="/favicon.ico">
+<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="apple-touch-icon-precomposed" href="img/apple-touch-icon-precomposed.png"/>
+<link href="./css/base.css" rel="stylesheet"/>
+<link href="./css/top.css" rel="stylesheet"/>
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300;400;500;700&display=swap" rel="stylesheet"> 
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-D3RYS991XQ"></script>
+<script>
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date()); 
+gtag('config', 'G-D3RYS991XQ');
+</script>
+</head>
+
+<body id="home">
+<?php include "inc/header.php"; ?>
+<article class="main">
+  <section id="mvarea">
+      <img src="./img/common/mv.jpg" alt="千代田写真" class="pc"><img src="./img/common/mv_sp.jpg" alt="千代田写真" class="sp">
+      <p>写真協力：千代田区広報広聴課</p>
+  </section>
+  <section id="news">
+    <div class="wrap">
+      <h2><strong><img src="./img/ttl/ttl_news.png" alt="News"></strong></h2>
+          <!--↓畳む内容が無い場合はfaq2、div内のaごとを複製しても、あるいはdivごと複製してもOK-->
+
+<!-お知らせ追加位置-->
+　　　　<!--/faqここまで-->
+          <dl class="faq">
+            <dt>
+               <span class="date">2026/03/31</span>
+              <p>３月２９日（日）に運行しましたシャトルバス「さくら祭り号」のトラブルについて</p>
+            </dt>
+            <dd>
+		<b>３月２９日（日）に運行しましたシャトルバス「さくら祭り号」のトラブルに伴い時刻とおりに運行が出来なくなったことで関係者の皆様やご利用された皆様に大変ご迷惑をお掛けしたことをお詫びいたします。</b><br>
+            </dd>
+          </dl>
+          <dl class="faq">
+            <dt>
+               <span class="date">2026/03/19</span>
+              <p>『千代田さくら祭り2026公式ガイドMAP』の訂正とお詫びについて</p>
+            </dt>
+            <dd>
+		<b>P04「千代田さくら祭り全体MAP」に記載の「菱東産業」様の所在地が誤っておりました。<br>
+                   関係者の皆様や読者の皆様には大変ご迷惑をお掛けしたことをお詫び申し上げます。<p>
+
+                   正式には「東京都千代田区一番町１９　全国農業共済会館」所在地は<a href="pdf/owabi_2026.pdf" target="_blank">添付のとおり</a>です。</b><br>
+            </dd>
+          </dl>
+          <dl class="faq">
+            <div class="faq2">
+            <a href="https://note.com/kanmachi" target="_blank">
+              <span class="date">2026/03/11</span><p>『千代田観光まちづくり公式note』開設！<br>千代田さくら祭り2026公式ガイドMAPやイベントに関する情報はこちらからご確認いただけます。<br>
+		</p>
+            </a>
+        </div>
+            <div class="faq2">
+            <a href="./guidemap2026/index.html" target="_blank">
+              <span class="date">2026/03/11</span><p>『千代田さくら祭り2026公式ガイドMAPデジタルブック版』はこちらからご覧いただけます。<br>
+		</p>
+            </a>
+        </div>
+            <dt>
+               <span class="date">2025/10/20</span>
+              <p>千代田さくら祭り2026公式ガイドMAP　掲載広告（新規）募集のご案内</p>
+            </dt>
+            <dd>
+		<b>千代田さくら祭り2026公式ガイドMAP　掲載広告（新規）募集のご案内</b><br>
+                問合せ先：千代田観光まちづくり実行委員会<br />
+                Tel　03-5244-5135　Fax　03-5244-5136　Email　kanmachi2@chiyoda-shokoren.com<br><br>
+
+		<b>千代田さくら祭り2026公式ガイドMAP</b><br>
+		媒体の概要については、<a href="pdf/sakura2026.pdf" target="_blank">千代田さくら祭り2026公式ガイドMAP　掲載広告募集のご案内「PDF版」</a>をご覧ください。<br>
+	        広告掲載料金、掲載サイズについては、<a href="pdf/sakura2026_guide.pdf" target="_blank">こちらのPDF</a>をご覧ください。<br>
+		広告掲載に関するお問い合わせ、お申込みは、<a href="pdf/sakura2026_moushikomi.pdf" target="_blank">広告掲載に関する連絡及びお申込み用紙「PDF版」</a>をご利用ください。<br>
+		
+            </dd>
+          </dl>
+
+　　　 <div class="faq2">
+            <a href="pdf/sakura2025kotae.pdf" target="_blank">
+              <span class="date">2025/05/15</span><p>「千代田さくら祭り2025公式ガイドMAP」に掲載の「千代田の謎」（P20-P21）の答えはこちらです！<br>
+		千代田の謎の答え（PDF）</p>
+            </a>
+        </div>
+	<dl class="faq">
+            <dt>
+               <span class="date">2025/03/11</span>
+              <p>「ちよだよりみちさんぽ2025」サイトを公開しました</p>
+            </dt>
+            <dd>
+              昨年に引き続き、千代田区内の大学生が「千代田さくら2025ガイドMAP」に掲載するコンテンツとしてサイトを企画しました。<br />
+詳しくは<a href="https://kanmachi.com/yorimichi2025/" target="_blank">「ちよだよりみちさんぽ2025」サイト</a>をご覧ください。
+            </dd>
+          </dl>
+        <!--/faqここまで-->
+
+	<dl class="faq">
+            <dt>
+               <span class="date">2025/03/11</span>
+              <p>無料シャトルバスさくら祭り号の運行、千代田さくら祭り2025公式ガイドMAP配布場所のご案内</p>
+            </dt>
+            <dd>
+		<b>無料シャトルバスさくら祭り号の運行</b><br>
+              <a href="pdf/sakura_bus2025.pdf" target="_blank">3月29日（土）・30日（日）の二日間「丸の内シャトルバス」のコースを延伸し「さくら祭り号」を運行</a>します。乗り降り自由ですのでぜひ千代田区内の回遊にご利用ください！<br /><br />
+		<b>千代田さくら祭り2025公式ガイドMAP</b><br>
+		毎年20万部を発行している<a href="pdf/sakura_bus2025.pdf" target="_blank">千代田さくら祭り2025公式ガイドMAPを今年も配布</a>します。<br><br>
+		さくら祭り号、公式ガイドMAPの詳しい情報は<a href="pdf/sakura_bus2025.pdf" target="_blank">こちら</a>からご覧いただけます。<br>
+		※時刻表はあくまで目安です。道の混雑状況により大幅に遅れる可能性がございます。<br>
+		※配布場所ですでに配布終了の場合はご了承ください。
+            </dd>
+          </dl>
+        <!--/faqここまで-->
+	<div class="faq2">
+            <a href="./guidemap2025/index.html" target="_blank">
+              <span class="date">2025/03/11</span><p>『千代田さくら祭り2025公式ガイドMAPデジタルブック版』はこちらからご覧いただけます。<br>
+		</p>
+            </a>
+        </div>
+
+	<div class="faq2">
+            <a href="pdf/sakura2025.pdf" target="_blank">
+              <span class="date">2024/11/22</span><p>千代田さくら祭り2025公式ガイドMAP　掲載広告（新規）募集のご案内<br>
+		問合せ先：千代田観光まちづくり実行委員会<br>
+		構成団体：東京商工会議所千代田支部／千代田区観光協会／千代田区商工業連合会／千代田区商店街連合会<br>
+		Tel　03-5244-5135　Fax　03-5244-5136　Email　kanmachi2@chiyoda-shokoren.com（pdf）</p>
+            </a>
+        </div>
+
+	<div class="faq2">
+            <a href="pdf/timetravelers.pdf" target="_blank">
+              <span class="date">2024/11/05</span><p>（第５回千代田お弁当大賞）<br>
+「デジタルスタンプラリー、千代田タイムトラベラーズ・ツアー2024-2025グルメ編」を開催！<br>
+2024年11月6日（水）～2025年2月5日（水）（pdf）</p>
+            </a>
+        </div>
+<!--
+	<dl class="faq">
+            <dt>
+               <span class="date">2024/03/12</span>
+              <p>『千代田のさくら祭り2024公式ガイドMAP』東京国立近代美術館『美術館の春まつり』頁について</p>
+            </dt>
+            <dd>
+		2024年3月発行の『千代田のさくら祭り2024公式ガイドMAP』の次の部分に訂正がございます。<br>
+              【該当箇所】東京国立近代美術館『美術館の春まつり』<br />
+			「中平卓馬　火―氾濫」観覧料金箇所<br />
+		【誤】※高校生以下および18歳未満、<span style="border-bottom: solid 1px red;">65歳以上</span>、障害者手帳をお持ちの方とその付添者（1名）は無料<br>
+		【正】 ※高校生以下および18歳未満、障害者手帳をお持ちの方とその付添者（1名）は無料<br>
+		詳細は<a href="https://www.momat.go.jp/topics/20240311">東京国立近代美術館の公式ホームページ</a>をご覧ください。
+            </dd>
+          </dl>
+
+	<dl class="faq">
+            <dt>
+               <span class="date">2024/03/12</span>
+              <p>『千代田のさくら祭り2024公式ガイドMAP』について</p>
+            </dt>
+            <dd>
+		2024年3月発行の『千代田のさくら祭り2024公式ガイドMAP』の次の部分に訂正がございます。<br>
+              謹んでお詫び申し上げますとともに、ここに訂正いたします。（2024年3月12日）<br />
+		【頁/位置】23頁/上段1行目<br>
+		【誤】千鳥ヶ淵宴会会場<br>
+		【正】千鳥ヶ淵会場周辺<br>
+		「なお、「千鳥ヶ淵公園」では3月22日（金）～3月24日（日）各日ともに11：00～18：00まで飲食ブースを出店しています。
+          </dl>
+
+	<dl class="faq">
+            <dt>
+               <span class="date">2024/03/08</span>
+              <p>「ちよだよりみちさんぽ2024」サイトを公開しました</p>
+            </dt>
+            <dd>
+              昨年に引き続き、千代田区内の大学生が「千代田さくら2024ガイドMAP」に掲載するコンテンツとしてサイトを企画しました。<br />
+詳しくは<a href="https://kanmachi.com/yorimichi2024/" target="_blank">「ちよだよりみちさんぽ2024」サイト</a>をご覧ください。
+            </dd>
+          </dl>
+
+	<dl class="faq">
+            <dt>
+               <span class="date">2024/03/06</span>
+              <p>無料シャトルバスさくら祭り号の運行、千代田さくら祭り2024公式ガイドMAP配布場所のご案内</p>
+            </dt>
+            <dd>
+		<b>無料シャトルバスさくら祭り号の運行</b><br>
+              3月23日（土）・24日（日）の二日間「丸の内シャトルバス」のコースを延伸し「さくら祭り号」を運行します。乗り降り自由ですのでぜひ千代田区内の回遊にご利用ください！<br /><br />
+		<b>千代田さくら祭り2024公式ガイドMAP</b><br>
+		毎年20万部を発行している千代田さくら祭り2024公式ガイドMAPを今年も配布します。<br>
+		千代田区内のMAPやさくら散策のおすすめコース、お店の情報など情報盛りだくさんです。（3月12日ごろから順次配布）ぜひ散策にお役立てください。<br><br>
+		さくら祭り号、公式ガイドMAPの詳しい情報は<a href="pdf/sakura_bus.pdf" target="_blank">こちら</a>からご覧いただけます。<br>
+		※時刻表はあくまで目安です。道の混雑状況により大幅に遅れる可能性がございます。<br>
+		※配布場所ですでに配布終了の場合はご了承ください。
+            </dd>
+          </dl>-->
+<!--	<dl class="faq">
+            <dt>
+               <span class="date">2021/08/30</span>
+              <p>「第2回千代田お弁当大賞」事務局からのお知らせ<br />コロナ禍の緊急事態宣言の延長に伴い、参加店申込締め切りを9月17日まで延長します。</p>
+            </dt>
+            </dl>
+
+<!--		<dl class="faq">
+            <dt>
+               <span class="date">2021/08/05</span>
+              <p>「第２回千代田お弁当大賞」サイトのご案内【参加店募集】</p>
+            </dt>
+            <dd>
+              飲食店の皆さまがコロナ禍で新たなビジネスチャンスとして生み出したテイクアウトお弁当にフォーカスして、千代田区内の飲食店を美味しく応援するイベントを今年も開催します。
+詳しくは<a href="https://www.kanmachi.com/obento2021entry/" target="_blank">「第２回千代田お弁当大賞」サイト</a>をご覧ください。
+            </dd>
+          </dl>-->
+
+<!--        <div class="faq2">
+            <a href="pdf/sakura2021map_20201224.pdf" target="_blank">
+              <span class="date">2020/12/14</span><p>「千代田さくら祭り 2021 公式ガイド MAP」発行中止のご連絡（pdf）</p>
+            </a>
+        </div>-->
+        <!--/faq2ここまで-->
+        <!--↓畳む内容がある場合はfaq、dl内のdt、ddごとを複製しても、あるいはdlごと複製してもOK-->
+<!--       <dl class="faq">
+            <dt>
+               <span class="date">2020/10/09</span>
+              <p>お弁当大賞サイトのご案内</p>
+            </dt>
+            <dd>
+              千代田区内の飲食店を応援します。<br>
+新型コロナウイルスの蔓延で窮地に立たされた飲食店の皆様が、感染拡大を防ぎながら新たなビジネスチャンスとして生み出したテイクアウトのお弁当にフォーカスして、千代田区内の飲食店を美味しく応援するイベントです。<br>
+詳しくは<a href="https://kanmachi.com/obento/" target="_blank">お弁当大賞サイト</a>をご覧ください。
+            </dd>
+          </dl>-->
+        <!--/faqここまで-->
+         <!--↓畳む内容が無い場合はfaq2、div内のaごとを複製しても、あるいはdivごと複製してもOK-->
+<!--         <div class="faq2">
+            <a href="pdf/tenpo_20200612.pdf" target="_blank">
+              <span class="date">2020/10/09</span><p>ガイドMAP掲載店舗のテイクアウト＆デリバリー対応店舗リストはこちら（pdf）</p>
+            </a>
+        </div>-->
+        <!--/faq2ここまで-->
+        <hr>
+    </div>
+  </section>
+  <section id="event" class="bg_g">
+    <div class="wrap">
+      <h2><strong><img src="./img/ttl/ttl_event.png" alt="開催中のイベント一覧"></strong></h2>
+        <!--↓イベント画像はpngでもjpgでもご自由に-->
+        <ul class="flex">
+        <li><a href="https://kanmachi.com/guidemap2025/index.html#page=1" target="_blank"><img src="./img/event/sakuramap2025.png" alt="千代田さくら2025ガイドMAP">
+            <h4>千代田さくら祭り2025公式ガイドMAP</h4>
+                <p>2025年3月～4月</p></a>
+        </li>
+	<li><a href="https://kanmachi.com/yorimichi2025/" target="_blank"><img src="./img/event/yorimichi2025.png" alt="ちよだよりみちさんぽ2025">
+            		<h4>ちよだよりみちさんぽ2025</h4>
+    		        <p></p></li>
+		<li><a href="pdf/timetravelers.pdf" target="_blank"><img src="./img/event/obento2024.png" alt="第5回千代田お弁当大賞2024">
+            		<h4>（第５回千代田お弁当大賞）<br>「デジタルスタンプラリー、千代田タイムトラベラーズ・ツアー2024-2025グルメ編」</h4>
+		<p>2024年11月6日～2025年2月5日</p></a>
+    		        <p></p></li>
+
+	    <li><a href="https://www.kanmachi.com/guidemap/" target="_blank"><img src="./img/event/sakuramap2024.jpg" alt="千代田さくら2024ガイドMAP">
+            <h4>千代田さくら祭り2024公式ガイドMAP</h4>
+                <p>2024年3月～4月</p></a></li>
+
+ 		<li><a href="https://kanmachi.com/yorimichi2024/" target="_blank"><img src="./img/event/yorimichi2024.png" alt="ちよだよりみちさんぽ2024">
+            		<h4>ちよだよりみちさんぽ2024</h4>
+		<!-- <p>2021年10月15日?12月20日</p></a>-->
+    		        <p></p></li>
+
+
+            <li><a href="https://www.kanmachi.com/obento2023/" target="_blank"><img src="./img/event/obento2023.png" alt="千代田お弁当大賞2023">
+            <h4>第4回千代田お弁当大賞</h4>
+                <p>2023年10月25日～11月27日</p></a></li>
+
+            <li><a href="pdf/ChiyodaSakuraFes2023.pdf" target="_blank"><img src="./img/event/sakuramap2023.jpg" alt="千代田さくら2023ガイドMAP">
+            <h4>千代田さくら祭り2023公式ガイドMAP</h4>
+                <p>2023年3月～4月</p></a></li>
+
+ 		<li><a href="https://kanmachi.com/yorimichi2023/" target="_blank"><img src="./img/event/yorimichi2023.png" alt="ちよだよりみちさんぽ2023">
+            		<h4>ちよだよりみちさんぽ2023</h4>
+		<!-- <p>2021年10月15日?12月20日</p></a>-->
+    		        <p></p></li>
+
+            <li><a href="https://www.kanmachi.com/obento2022/" target="_blank"><img src="./img/event/obento2022.png" alt="千代田お弁当大賞2022">
+            <h4>第3回千代田お弁当大賞</h4>
+                <p>2022年10月28日$301C11月23日</p></a></li>
+
+            <li><a href="pdf/sakura2022map.pdf" target="_blank"><img src="./img/event/sakuramap2022.jpg" alt="千代田さくら2022ガイドMAP">
+            <h4>千代田さくら2022ガイドMAP</h4>
+                <p>2022年3月～4月</p></a></li>
+            <li><a href="https://kanmachi.com/yorimichi/" target="_blank"><img src="./img/event/yorimichi.jpg" alt="ちよだよりみちさんぽ">
+            		<h4>ちよだよりみちさんぽ</h4>
+		<!-- <p>2021年10月15日?12月20日</p></a>-->
+    		        <p></p></li>
+            <li><a href="https://www.kanmachi.com/obento/" target="_blank"><img src="./img/event/obento2021_1.jpg" alt="第2回千代田お弁当大賞">
+            <h4>第2回千代田お弁当大賞</h4>
+		 <p>2021年10月15日～12月20日</p></a>
+            <p></p></li>
+            <li><img src="./img/event/bnn_event001.png" alt="お弁当大賞">
+            <h4>千代田お弁当大賞</h4>
+            <p>2020年10月9日～12月10日</p></a></li>
+            <li><a href="http://www.chiyoda-event.com/kanmachi/" target="_blank"><img src="./img/event/bnn_event002.png" alt="千代田区のさくら祭り">
+            <h4>千代田区のさくら祭り2020</h4>
+                <p>2020年3月～4月</p></a></li>
+        </ul>
+    </div>
+  </section>
+  <section id="top_about" class="bg_p"><a href="about.php">
+    <div class="wrap">
+     <p><img src="./img/ttl/ttl_about_pc.png" alt="千代田観光まちづくり実行委員会とは" class="pc"><img src="./img/ttl/ttl_about_sp.png" alt="千代田観光まちづくり実行委員会とは" class="sp"></p>
+    </div></a>
+  </section>
+  <section id="top_inq">
+    <div class="wrap bg_gr">
+      <h2><strong><img src="./img/ttl/ttl_inquiry.png" alt="お問い合わせ"></strong></h2>
+        <div class="btnarea">
+        <a href="/inquiry/contact.php" class="btn">お問い合わせフォームはこちら</a></div>
+    </div>
+  </section>
+</article>
+<?php include "inc/footer.php"; ?>
+<script src="./js/jquery-1.7.1.min.js"></script> 
+<script src="./js/common.js"></script>
+</body>
+</html>
+
