c95d7f5a99c0004e284de5bf9b0c5ac015e2f7ad
Toyobijutsu-Prt <hatanaka@toyobijutsu-prt.co.jp>
2026-09-11 10:57:16 +0900

    例後修正

---
+++ libs/mailvars.php
@@ -0,0 +1,9 @@
+<?php
+//メールの宛先（To）の Email アドレス
+define( 'MAIL_TO', "kanmachi2@chiyoda-shokoren.com" );
+//メールの宛先（To）の名前  
+define( 'MAIL_TO_NAME', "千代田観光まちづくり実行委員会窓口" );
+//Return-Pathに指定するメールアドレス
+define( 'MAIL_RETURN_PATH', 'kanmachi2@chiyoda-shokoren.com' );
+//自動返信の返信先名前（自動返信を設定する場合）
+define( 'AUTO_REPLY_NAME', '千代田観光まちづくり実行委員会窓口' );
--- page-about.php
+++ page-about.php
@@ -16,40 +16,44 @@ get_header( null, $header_args );
         <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/ttl/ttl_about_pc.png' ); ?>"  alt="千代田観光まちづくり実行委員会とは" class="pc">
         <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/ttl/ttl_about_sp.png' ); ?>" alt="千代田観光まちづくり実行委員会とは" class="sp">
       </h2>
-      <?php if( have_rows('about_contents') ): ?>
-        <?php while ( have_rows('about_contents') ) : the_row(); ?>
-          <?php if( get_row_layout() == 'about_contents1' ): ?>
-            <p><?php the_sub_field('about_text'); ?></p>
-          <?php endif; ?>
-          <?php if( get_row_layout() == 'about_contents2' ): ?>
-            <ul class="flex">
-              <li>
-                <img src="<?php the_sub_field('about_image2'); ?>" alt="<?php the_sub_field('about_image_alt'); ?>">
-              </li>
-              <li>
-                <p><?php the_sub_field('about_text'); ?></p>
-              </li>
-            </ul>
-          <?php endif; ?>
-        <?php endwhile; ?>
-      <?php endif; ?>
-    </div>
-        
-    <div class="info">
-      <strong>千代田観光まちづくり実行委員会</strong><br>
-      <ul>
-        <li>構成団体</li>
-        <li>東京商工会議所千代田支部<br>
-          （一社）千代田区観光協会<br>
-          千代田区商工業連合会<br>
-          千代田区商店街連合会</li>
-        <li class="bdr_d1"></li>
-        <li class="bdr_d2"></li>
-        <li>事務局</li>
-        <li>千代田区商工業連合会内</li>
-        <li>所在地</li>
-        <li>〒101-0054　千代田区神田錦町3-21　ちよだプラットフォームスクウェア4F</li>
-      </ul>
+      <div class="intro">
+        <?php if( have_rows('about_contents') ): ?>
+          <?php $about_image_index = 0; ?>
+          <?php while ( have_rows('about_contents') ) : the_row(); ?>
+            <?php if( get_row_layout() == 'about_contents1' ): ?>
+              <p><?php the_sub_field('about_text'); ?></p>
+            <?php endif; ?>
+            <?php if( get_row_layout() == 'about_contents2' ): ?>
+              <ul class="flex<?php echo 1 === $about_image_index % 2 ? ' reverse' : ''; ?>">
+                <li>
+                  <img src="<?php the_sub_field('about_image2'); ?>" alt="<?php the_sub_field('about_image_alt'); ?>">
+                </li>
+                <li>
+                  <p><?php the_sub_field('about_text'); ?></p>
+                </li>
+              </ul>
+              <?php $about_image_index++; ?>
+            <?php endif; ?>
+          <?php endwhile; ?>
+        <?php endif; ?>
+      </div>
+
+      <div class="info">
+        <strong>千代田観光まちづくり実行委員会</strong><br>
+        <ul>
+          <li>構成団体</li>
+          <li>東京商工会議所千代田支部<br>
+            （一社）千代田区観光協会<br>
+            千代田区商工業連合会<br>
+            千代田区商店街連合会</li>
+          <li class="bdr_d1"></li>
+          <li class="bdr_d2"></li>
+          <li>事務局</li>
+          <li>千代田区商工業連合会内</li>
+          <li>所在地</li>
+          <li>〒101-0054　千代田区神田錦町3-21　ちよだプラットフォームスクウェア4F</li>
+        </ul>
+      </div>
     </div>
   </section>
 
