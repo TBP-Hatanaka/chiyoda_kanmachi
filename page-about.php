9ed97244f9242b6f61ef9055945c3c72ef917e90
Toyobijutsu-Prt <hatanaka@toyobijutsu-prt.co.jp>
2026-09-11 17:18:19 +0900

    更新

--- page-about.php
+++ page-about.php
@@ -25,10 +25,11 @@ get_header( null, $header_args );
             <?php if( get_row_layout() == 'about_contents2' ): ?>
               <div class="flex">
                 <div class="intro_img">
-                  <img class="about_image" src="<?php the_sub_field('about_image2'); ?>" alt="<?php the_sub_field('about_image_alt'); ?>">
+                  <img class="about_image1" src="<?php the_sub_field('about_image2'); ?>" alt="<?php the_sub_field('about_image_alt'); ?>">
+                  <img class="about_image2" src="<?php the_sub_field('about_image2'); ?>" alt="<?php the_sub_field('about_image_alt'); ?>">
                 </div>
                 <div class="intro_txt">
-                  <?php the_sub_field('about_text'); ?>
+                  <p><?php the_sub_field('about_text'); ?></p>
                 </div>
               </div>
             <?php endif; ?>
