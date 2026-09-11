5d3365bd321d1ce4f3747b7694e6161670ec02c0
Toyobijutsu-Prt <hatanaka@toyobijutsu-prt.co.jp>
2026-09-11 17:42:39 +0900

    更新

--- page-about.php
+++ page-about.php
@@ -36,49 +36,6 @@ get_header( null, $header_args );
           <?php endwhile; ?>
         <?php endif; ?>
       </div>
-
-  <section id="container">
-    <div class="wrap">
-      <h2>
-        <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/ttl/ttl_about_pc.png' ); ?>"  alt="千代田観光まちづくり実行委員会とは" class="pc">
-        <img src="<?php echo esc_url( get_stylesheet_directory_uri() . '/img/ttl/ttl_about_sp.png' ); ?>" alt="千代田観光まちづくり実行委員会とは" class="sp">
-      </h2>
-      <div class="intro">
-        <?php if( have_rows('about_contents') ): ?>
-          <?php while ( have_rows('about_contents') ) : the_row(); ?>
-            <?php if( get_row_layout() == 'about_contents1' ): ?>
-              <p><?php the_sub_field('about_text'); ?></p>
-            <?php endif; ?>
-            <?php if( get_row_layout() == 'about_contents2' ): ?>
-              <ul class="flex">
-                <li>
-                  <img class="about_image" src="<?php the_sub_field('about_image2'); ?>" alt="<?php the_sub_field('about_image_alt'); ?>">
-                </li>
-                <li>
-                  <?php the_sub_field('about_text'); ?>
-                </li>
-              </ul>
-            <?php endif; ?>
-          <?php endwhile; ?>
-        <?php endif; ?>
-      </div>
-
-      <div class="info">
-        <strong>千代田観光まちづくり実行委員会</strong><br>
-        <ul>
-          <li>構成団体</li>
-          <li>東京商工会議所千代田支部<br>
-            （一社）千代田区観光協会<br>
-            千代田区商工業連合会<br>
-            千代田区商店街連合会</li>
-          <li class="bdr_d1"></li>
-          <li class="bdr_d2"></li>
-          <li>事務局</li>
-          <li>千代田区商工業連合会内</li>
-          <li>所在地</li>
-          <li>〒101-0054　千代田区神田錦町3-21　ちよだプラットフォームスクウェア4F</li>
-        </ul>
-      </div>
     </div>
   </section>
 </article>
